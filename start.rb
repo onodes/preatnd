fae3f89985825815bde78d888f7f55b68aec18cf
onodes <onodes@gmail.com>
2009-12-10 17:47:28 +0900

    add start.rb

---
+++ start.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+require 'model/comment.rb'
+ 
+helpers do
+  include Rack::Utils; alias_method :h, :escape_html
+end
+ 
+get '/style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :style
+end
+ 
+get '/' do
+  @comments = Comments.order_by(:posted_date.desc)
+  haml :index
+end
+ 
+put '/comment' do
+  Comments.create({
+    :name => request[:name],
+    :title => request[:title],
+    :message => request[:message],
+    :posted_date => Time.now,
+  })
+  redirect '/'
+end
+
